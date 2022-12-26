ea5efa87ba5fda0e0c257af6548f1324903f1933
liuhui <3219748434@qq.com>
2022-12-26 20:08:22 +0800

    测试

---
+++ 11.4.c/11.4.c/11.4.c.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int i = 0;
+	int count = 0;
+	scanf("%d", &a);
+	for (i = 0; i < 32; i++)
+	{
+		if(1==((a >> 1) & 1))
+			count++;
+	}
+	printf("%d\n", count);
+	return 0;
+}
